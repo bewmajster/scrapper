375282a8763ce08002824d7121ff5b208c285491
Bew Majster <bew.majster@gmail.com>
2022-06-22 13:56:07 +0200

    Improved port definition

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
 const http = require("http");
+const onPort = process.env.PORT || 3000;
 
 var app = http.createServer(function (req, res) {
   res.setHeader("Content-Type", "application/json");
   res.end(fs.readFileSync("./hodnotenia.json", { encoding: "utf8" }));
 });
 
-app.listen(process.env.PORT);
+app.listen(onPort);
